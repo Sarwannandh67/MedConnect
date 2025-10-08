533047c95ed96c6db8f283e2ba6b1f1116ed1d3d
supradeepp <supradeepp@users.noreply.github.com>
2025-10-08 16:00:18 +0530

    Initial commit

---
+++ OnlineMedicineDonationSystem.java
@@ -0,0 +1,8 @@
+package online.medicine.donation.system;
+
+public class OnlineMedicineDonationSystem {
+    public static void main(String[] args) {
+        HomePage frame = new HomePage();
+        frame.setVisible(true);
+    }
+}
